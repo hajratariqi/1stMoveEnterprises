62219de921ea2b9970cc947afe61271cf51d63fd
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-12 15:58:20 +0500

    done responsiveness

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,11 +7,11 @@ const Banner = () => {
     <div className='relative w-full'>
 
             <img src={business} alt="banner" className='w-full h-[70vh] object-cover'/>
-            <div className='inset-0 absolute bg-black opacity-25 h-full'></div>
+            <div className='inset-0 absolute bg-black opacity-30 h-full'></div>
 
-        <div className='max-w-2xl absolute text-white top-0 text-center w-full mx-auto left-0 right-0 flex flex-col justify-center items-center h-full'>
-            <h1 className='text-7xl font-bold capilize'>Make your 1stMove to bring success!</h1>
-            <p className='mt-10 text-lg'>One of the leading distributors of Pakistan dealing general goods, cosmetics, herbal products, garments and textiles good, mobile phones and vehicles.</p>
+        <div className='max-w-2xl absolute p-5 text-white top-0 text-center w-full mx-auto left-0 right-0 flex flex-col justify-center items-center h-full'>
+            <h1 className='text-5xl md:text-7xl font-bold capilize'>Make your 1stMove to bring success!</h1>
+            <p className='md:mt-10 mt-5 text-sm md:text-lg'>One of the leading distributors of Pakistan dealing general goods, cosmetics, herbal products, garments and textiles good, mobile phones and vehicles.</p>
         </div>
 
     </div>
