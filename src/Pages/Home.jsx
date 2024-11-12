5e5a9e346e33e9c0eb453996b9993310cfaa1d40
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-12 16:35:25 +0500

    Added in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from '../Components/About'
 import  Banner from '../Components/Banner'
+import MissonVisson from '../Components/MissonVisson'
 const Home = () => {
   return (
     <div>
         <Banner/>
         <About/>
+        <MissonVisson/>
     </div>
   )
 }
