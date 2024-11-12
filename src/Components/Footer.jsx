43595f6cfa07a835b836fb6d209e91727d5c6614
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-12 22:59:35 +0500

    fix social media links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -19,16 +19,16 @@ const Footer = () => {
                 </div>
 
             <div className='flex flex-wrap gap-5'>
-                <Link className='p-2 rounded-full bg-black text-white' to="https://www.facebook.com" target="_blank">
+                <Link className='p-2 rounded-full bg-black text-white' to="https://www.facebook.com/1stmoveenterprises" target="_blank">
                     <FaFacebook size={20} />
                 </Link>
-                <Link className='p-2 rounded-full bg-black text-white' to="https://www.instagram.com" target="_blank">
+                <Link className='p-2 rounded-full bg-black text-white' to="https://www.instagram.com/1stmoveenterprises" target="_blank">
                     <FaInstagram size={20} />
                 </Link>
-                <Link className='p-2 rounded-full bg-black text-white' to="https://twitter.com" target="_blank">
+                <Link className='p-2 rounded-full bg-black text-white' to="https://twitter.com/1stmoveenterprises" target="_blank">
                     <FaTwitter size={20} />
                 </Link>
-                <Link className='p-2 rounded-full bg-black text-white' to="https://www.linkedin.com" target="_blank">
+                <Link className='p-2 rounded-full bg-black text-white' to="https://www.linkedin.com/1stmoveenterprises" target="_blank">
                     <FaLinkedin size={20} />
                 </Link>
             </div>
@@ -36,7 +36,7 @@ const Footer = () => {
             </div>
         </div>
         <div className='border-t p-7 text-center flex items-center justify-between'>
-            <Link to='https://hybridinnovations.vercel.app/'><p className='font-normal mt-2'>Developed by <span className='text-black font-medium'>Hybrid Innovations</span> </p></Link>
+            <Link to='https://hybridinnovations.vercel.app/' target='_blank'><p className='font-normal mt-2'>Developed by <span className='text-black font-medium'>Hybrid Innovations</span> </p></Link>
             <p>1stmoveenterprises - © 2024 All Rights Reserved</p>
             <p>Terms & condition</p>
         </div>
