139dcd37c32ea2f3639cbc9523c63ffb9874b4c5
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-12 21:59:02 +0500

    header scroll issue fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,22 +2,23 @@ import Logo from '../assets/Logo.png';
 import { IoIosMenu } from "react-icons/io";
 import { IoCloseSharp } from "react-icons/io5";
 import React, { useState, useEffect} from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
     const location = useLocation();
 
-    useEffect(() => {
-        const handleScroll = () => {
-          if (open) {
-            setOpen(false);
-          }};
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, [open]);
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //       if (open) {
+    //         setOpen(false);
+    //       }};
+    //     window.addEventListener('scroll', handleScroll);
+    //     return () => {
+    //       window.removeEventListener('scroll', handleScroll);
+    //     };
+    //   }, [open]);
 
     const navItems = [
         { name: 'Home', path: '#home' },
@@ -40,7 +41,8 @@ const Header = () => {
         <ul className='hidden md:flex space-x-8 font-poppins text-lg'>
         {navItems.map(item => (
           <li key={item.path} className='font-medium'>
-            <Link smooth to={item.path}  onClick={() => window.scrollTo(0, 0)} className={`py-2 transition-all ${location.pathname === item.path ? 'bg-black px-4 rounded-sm text-white' : 'hover:text-gray-500-500'}`}>
+            {/* <Link smooth to={item.path}  onClick={() => window.scrollTo(0, 0)} className={`py-2 transition-all ${location.pathname === item.path ? 'bg-black px-4 rounded-sm text-white' : 'hover:text-gray-500-500'}`}> */}
+            <Link smooth to={item.path}>
               {item.name}
             </Link>
           </li>
