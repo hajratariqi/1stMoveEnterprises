825df52266ae5b0b0e2d76236a1afafada3ce832
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-13 17:22:47 +0500

    mobbile header fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,6 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
-    const location = useLocation();
 
     // useEffect(() => {
     //     const handleScroll = () => {
@@ -32,7 +31,6 @@ const Header = () => {
         <div className="container flex justify-between items-center h-full mx-auto">
         <div className='flex items-center'>
           <img src={Logo} alt="Logo" className='w-44 h-auto ' />
-            {/* <img src='https://framerusercontent.com/images/lg5HFPHgvIKQh0U35IwOnv5uqI.png' alt="Logo" className='w-56 h-auto ' /> */}
         </div>
 
         <div className='text-4xl cursor-pointer md:hidden z-20' onClick={() => setOpen(!open)}>
@@ -42,7 +40,6 @@ const Header = () => {
         <ul className='hidden md:flex space-x-8 font-poppins text-lg'>
         {navItems.map(item => (
           <li key={item.path} className='font-medium'>
-            {/* <Link smooth to={item.path}  onClick={() => window.scrollTo(0, 0)} className={`py-2 transition-all ${location.pathname === item.path ? 'bg-black px-4 rounded-sm text-white' : 'hover:text-gray-500-500'}`}> */}
             <Link smooth to={item.path}>
               {item.name}
             </Link>
@@ -53,11 +50,11 @@ const Header = () => {
 
         {/* mobbile header */}
         {open && (
-            <div className='h-100vh'>
-                <ul className='h-full mt-20 justify-center items-center text-center flex flex-col space-y-4 '>
+            <div className='h-screen z-40 absolute top-24 left-0 right-0 bg-white'>
+                <ul className='h-full mt-20  items-center text-center flex flex-col space-y-4 '>
                 {navItems.map(item => (
                     <li key={item.path} className='font-medium'>
-                        <Link smooth to={item.path}  onClick={() => window.scrollTo(0, 0)} className={`py-2 transition-all ${location.pathname === item.path ? 'bg-black px-4 rounded-sm text-white' : 'hover:text-gray-500-500'}`}>
+                        <Link smooth to={item.path}>
                         {item.name}
                         </Link>
                     </li>
